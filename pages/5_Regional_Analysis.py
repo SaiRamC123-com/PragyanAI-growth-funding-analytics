5aa57b043e01808f32f369452b5a1b4492c1bcc1
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 10:58:51 +0530

    Update 5_Regional_Analysis.py

--- pages/5_Regional_Analysis.py
+++ pages/5_Regional_Analysis.py
@@ -1,1 +1,27 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title("🌍 Regional Analysis")
+
+df = pd.read_csv("data/startup_data.csv")
+
+region_count = df["Region"].value_counts().reset_index()
+
+fig = px.pie(
+    region_count,
+    names="Region",
+    values="count"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+fig2 = px.bar(
+    df.groupby("Region")[
+        "Revenue (M USD)"
+    ].sum().reset_index(),
+    x="Region",
+    y="Revenue (M USD)"
+)
+
+st.plotly_chart(fig2,use_container_width=True)
