d4ceb1dcca40427c9542d24e608a3bdc41fa6c7f
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 10:52:24 +0530

    Update 2_Funding_Analytics.py

--- pages/2_Funding_Analytics.py
+++ pages/2_Funding_Analytics.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("💰 Funding Analysis")
+
+fig = px.histogram(
+    df,
+    x='Funding Amount (M USD)',
+    nbins=30
+)
+
+st.plotly_chart(fig)
+
+fig2 = px.box(
+    df,
+    x='Industry',
+    y='Funding Amount (M USD)',
+    color='Industry'
+)
+
+st.plotly_chart(fig2)
