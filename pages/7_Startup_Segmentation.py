8f227ad40332a9366a69c415654e03555961a466
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 10:59:18 +0530

    Update 7_Startup_Segmentation.py

--- pages/7_Startup_Segmentation.py
+++ pages/7_Startup_Segmentation.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title("🎯 Startup Segmentation")
+
+df = pd.read_csv("data/startup_data.csv")
+
+fig = px.scatter(
+    df,
+    x="Employees",
+    y="Revenue (M USD)",
+    color="Industry",
+    size="Valuation (M USD)"
+)
+
+st.plotly_chart(fig,use_container_width=True)
