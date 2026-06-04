8e549338998c828e6477b780339a3f65224e50fd
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 10:58:08 +0530

    Update 3_Valuation_Insights.py

--- pages/3_Valuation_Insights.py
+++ pages/3_Valuation_Insights.py
@@ -1,1 +1,30 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title("📈 Valuation Insights")
+
+df = pd.read_csv("data/startup_data.csv")
+
+top10 = df.nlargest(
+    10,
+    "Valuation (M USD)"
+)
+
+fig = px.bar(
+    top10,
+    x="Startup Name",
+    y="Valuation (M USD)",
+    color="Industry"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+fig2 = px.scatter(
+    df,
+    x="Revenue (M USD)",
+    y="Valuation (M USD)",
+    color="Industry"
+)
+
+st.plotly_chart(fig2,use_container_width=True)
