1cc32e8884034ee0f020720b3f319692fffff3de
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 10:50:57 +0530

    Update 1_Executive_Dashboard.py

--- pages/1_Executive_Dashboard.py
+++ pages/1_Executive_Dashboard.py
@@ -1,1 +1,30 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("🏭 Industry Analysis")
+
+industry_stats = df.groupby('Industry').agg({
+    'Funding Amount (M USD)':'sum',
+    'Revenue (M USD)':'sum',
+    'Valuation (M USD)':'mean'
+}).reset_index()
+
+fig = px.bar(
+    industry_stats,
+    x='Industry',
+    y='Funding Amount (M USD)',
+    color='Industry'
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig2 = px.treemap(
+    industry_stats,
+    path=['Industry'],
+    values='Revenue (M USD)'
+)
+
+st.plotly_chart(fig2, use_container_width=True)
