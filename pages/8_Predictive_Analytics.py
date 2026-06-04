548bef9397e8f5333fb374e19abc1a58eb8a3d34
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 10:55:45 +0530

    Update 8_Predictive_Analytics.py

--- pages/8_Predictive_Analytics.py
+++ pages/8_Predictive_Analytics.py
@@ -1,1 +1,36 @@
+import streamlit as st
+import pandas as pd
 
+from sklearn.ensemble import RandomForestRegressor
+
+df = pd.read_csv("data/startup_data.csv")
+
+X = df[
+[
+'Funding Amount (M USD)',
+'Revenue (M USD)',
+'Employees',
+'Market Share (%)'
+]
+]
+
+y = df['Valuation (M USD)']
+
+model = RandomForestRegressor()
+model.fit(X,y)
+
+st.title("🤖 Valuation Predictor")
+
+funding = st.number_input("Funding")
+revenue = st.number_input("Revenue")
+employees = st.number_input("Employees")
+market = st.number_input("Market Share")
+
+if st.button("Predict"):
+    pred = model.predict(
+        [[funding,revenue,employees,market]]
+    )[0]
+
+    st.success(
+        f"Estimated Valuation: ${pred:,.2f}M"
+    )
