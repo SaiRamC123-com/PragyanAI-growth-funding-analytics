6c21e8632040c13925b663ca2f7aee4cd10aee59
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 10:58:30 +0530

    Update 4_Industry_Analysis.py

--- pages/4_Industry_Analysis.py
+++ pages/4_Industry_Analysis.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title("🏭 Industry Analysis")
+
+df = pd.read_csv("data/startup_data.csv")
+
+fig = px.treemap(
+    df,
+    path=["Industry"],
+    values="Revenue (M USD)"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+industry = df.groupby("Industry").agg({
+    "Funding Amount (M USD)":"sum",
+    "Revenue (M USD)":"sum"
+})
+
+st.dataframe(industry)
