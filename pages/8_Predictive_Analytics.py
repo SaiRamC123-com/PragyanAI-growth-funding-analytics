1f8c17d3ff61d25433609f268505f0aab0c0ec6a
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-04 10:59:54 +0530

    Update 8_Predictive_Analytics.py

--- pages/8_Predictive_Analytics.py
+++ pages/8_Predictive_Analytics.py
@@ -3,34 +3,34 @@ import pandas as pd
 
 from sklearn.ensemble import RandomForestRegressor
 
+st.title("🤖 Predictive Analytics")
+
 df = pd.read_csv("data/startup_data.csv")
 
 X = df[
 [
-'Funding Amount (M USD)',
-'Revenue (M USD)',
-'Employees',
-'Market Share (%)'
+"Funding Amount (M USD)",
+"Revenue (M USD)",
+"Employees",
+"Market Share (%)"
 ]
 ]
 
-y = df['Valuation (M USD)']
+y = df["Valuation (M USD)"]
 
 model = RandomForestRegressor()
 model.fit(X,y)
-
-st.title("🤖 Valuation Predictor")
 
 funding = st.number_input("Funding")
 revenue = st.number_input("Revenue")
 employees = st.number_input("Employees")
 market = st.number_input("Market Share")
 
-if st.button("Predict"):
+if st.button("Predict Valuation"):
     pred = model.predict(
         [[funding,revenue,employees,market]]
     )[0]
 
     st.success(
-        f"Estimated Valuation: ${pred:,.2f}M"
+        f"Estimated Valuation: ${pred:.2f} M"
     )
